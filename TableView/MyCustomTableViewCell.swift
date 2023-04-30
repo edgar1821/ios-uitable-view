cc02435c70ce54b89538b829338f145a667345e8
Edgar <edgar.gamarra21@gmail.com>
2023-04-30 10:51:56 -0500

    celdas personalizadas

---
+++ TableView/MyCustomTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MyCustomTableViewCell.swift
+//  TableView
+//
+//  Created by Edgar Gamarra on 29/04/23.
+//
+
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myFirstLabel: UILabel!
+    @IBOutlet weak var mySecondLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    // esta funcion se ejecuta por primera vez
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        myFirstLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        myFirstLabel.textColor = .blue
+        backgroundColor = .gray
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        print(myFirstLabel.text ?? "")
+    }
+    
+}
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.tableFooterView = UIView()
+        
+        // registrando la nueva celda
+        tableView.register(UINib(nibName: "MyCustomTableViewCell", bundle: nil), forCellReuseIdentifier: "MyCustomCell")
     }
 
 
@@ -29,17 +32,41 @@ extension ViewController:UITableViewDataSource{
         return myCountries.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
-        if cell == nil {
-            cell = UITableViewCell(style: .default , reuseIdentifier: "myCell" )
-            cell?.backgroundColor = .gray
-            cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
-            cell?.accessoryType = .disclosureIndicator
+    //maneja el numero de secciones en el tableview
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 50
         }
         
-        cell?.textLabel?.text = myCountries[indexPath.row]
-        return cell!
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+            if cell == nil {
+                cell = UITableViewCell(style: .default , reuseIdentifier: "myCell" )
+                cell?.backgroundColor = .gray
+                cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+                cell?.accessoryType = .disclosureIndicator
+            }
+            
+            cell?.textLabel?.text = myCountries[indexPath.row]
+            return cell!
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath) as? MyCustomTableViewCell
+            
+            cell?.myFirstLabel.text = String(indexPath.row + 1)
+            cell!.mySecondLabel.text = myCountries[indexPath.row]
+            return cell!
+        }
+        
+        
     }
 }
 
